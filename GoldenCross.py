d0e1b5984af8cfc0287b5e7dea9736d563d021ab
willtsangaa <wltsangaa@connect.ust.hk>
2021-12-15 22:26:15 +0800

    first commit

---
+++ GoldenCross.py
@@ -0,0 +1,22 @@
+import math 
+import backtrader as bt
+
+
+class GoldenCross(bt.Strategy):
+
+	params = (('fast', 50), ('slow', 200), ('order_percentage', 0.95), ('ticker', 'SPY'))
+
+	def __init__(self):
+		self.fast_moving_average = bt.indicators.SMA(
+			self.data.close, period=self.params.fast, plotname='50 day moving average'
+		)
+
+		self.slow_moving_average = bt.indicators.SMA(
+			self.data.close, period=self.params.slow, plotname='200 dat moving average'
+		)
+		
+
+		self.corssover = bt.indicators.CrossOver(self.fast_moving_average, self.slow_moving_average)
+
+	def next(self):
+		pass
